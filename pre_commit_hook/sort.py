ea07035b7e738ff45b053f7ae55d929920fc5c59
James Seden Smith <sedders123@users.noreply.github.com>
2018-11-28 20:26:06 +0000

    Simplify for my use case

--- pre_commit_hook/sort.py
+++ pre_commit_hook/sort.py
@@ -6,32 +6,16 @@ import os
 from isort import isort
 
 
-def imports_incorrect(filename, show_diff=False):
-    return isort.SortImports(filename, check=True, show_diff=show_diff).incorrectly_sorted
+def imports_incorrect(filename):
+    return isort.SortImports(filename, check=True).incorrectly_sorted
 
 
 def main(argv=None):
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='Filenames to run')
-    parser.add_argument('--silent-overwrite', action='store_true', dest='silent', default=False)
-    parser.add_argument('--check-only', action='store_true', dest='check_only', default=False)
-    parser.add_argument('--diff', action='store_true', dest='show_diff', default=False)
-    args = parser.parse_args(argv)
-
-    return_value = 0
-
     for filename in args.filenames:
-        if imports_incorrect(filename, show_diff=args.show_diff):
-            if args.check_only:
-                return_value = 1
-            elif args.silent:
-                isort.SortImports(filename)
-            else:
-                return_value = 1
-                isort.SortImports(filename)
-                print('FIXED: {0}'.format(os.path.abspath(filename)))
-    return return_value
+        if imports_incorrect(filename):
+            isort.SortImports(filename)
+            print('FIXED: {0}'.format(os.path.abspath(filename)))
+    return 0
 
 if __name__ == '__main__':
     exit(main())
